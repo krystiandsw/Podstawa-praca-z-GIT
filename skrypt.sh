87772a5bfd928d20a5f3994dd9e64dd1d7b65fb0
krystiandsw <mailbox08743@gmail.com>
2025-05-05 22:55:53 +0200

    dodano --init

--- skrypt.sh
+++ skrypt.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
-if [ "$1" == "--date" ]; then
+if [ "$1" == "--date" ] || [ "$1" == "-d" ]; then
 	date
+elif [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
+	count=100
+	if [ -n "$2" ]; then
+		count=$2
+	fi
+
+	for i in $(seq 1 $count); do
+		echo "Nazwa pliku: log$i.txt" > log$i.txt	
+		echo "Nazwa skryptu: $0" >> log$i.txt
+		echo "Data: $(date)" >> log$i.txt
+	done
+elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+	echo "Dostepne opcje:"
+	echo "--date (-d) - wyswietla date"
+	echo "--logs (-l) [liczba] - tworzy pliki log (domyslny 100)"
+	echo "--help (-h) - wyswietla pomoc"
+elif [ "$1" == "--init" ]; then
+	repo_url="https://github.com/krystiandsw/Podstawa-praca-z-GIT.git"
+	git clone $repo_url
+	echo "export PATH=\$PATH:$(pwd)" >> ~/.bashrc
+	source ~/.bashrc
+	echo "Repozytorium sklonowane i sciezka dodana do PATH"
 fi
