7b97365e7e8467c447275219d164d608c4f9fc9c
krystiandsw <mailbox08743@gmail.com>
2025-05-05 22:51:42 +0200

    dodano --logs

--- skrypt.sh
+++ skrypt.sh
@@ -2,4 +2,15 @@
 
 if [ "$1" == "--date" ]; then
 	date
+elif [ "$1" == "--logs" ]; then
+	count=100
+	if [ -n "$2" ]; then
+		count=$2
+	fi
+
+	for i in $(seq 1 $count); do
+		echo "Nazwa pliku: log$i.txt" > log$i.txt	
+		echo "Nazwa skryptu: $0" >> log$i.txt
+		echo "Data: $(date)" >> log$i.txt
+	done
 fi
